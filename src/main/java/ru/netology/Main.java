cb0840e763a05eb3cf2347c0c4c1eaa1490704aa
“Maxim <kizilov2016@yandex.ru>
2024-07-06 13:01:15 +1000

    Initial commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -20,7 +20,16 @@ public class Main {
                 final var server = new Server(clientSocket);
                 server.addHandler("GET", "/messages", new Handler() {
                    public void handle(Request request, BufferedOutputStream responseStream) {
-//                        // TODO: handlers code
+                        final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
                     }
                 });
                 executorService.execute(new Server(clientSocket));
