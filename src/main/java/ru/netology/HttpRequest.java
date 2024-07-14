84ce52383888c5012f9c421880687845f4c473d6
“Maxim <kizilov2016@yandex.ru>
2024-07-15 00:04:59 +1000

    Initial commit

--- src/main/java/ru/netology/HttpRequest.java
+++ src/main/java/ru/netology/HttpRequest.java
@@ -1,69 +1,8 @@
 package ru.netology;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class HttpRequest {
-    private final static String HEADER_DELIMITER = ":";
+public record HttpRequest(HttpMethod methodType, String path, Map<String, String> heading, String body) {
 
 
-    private HttpMethod methodType;
-    private String path;
-    private final Map<String, String> heading;
-    private String body;
-
-    public HttpRequest(Socket socket) {
-        try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            String requestLine = in.readLine();
-            if (requestLine != null) {
-                String[] requestComponents = requestLine.split(" ");
-                methodType = HttpMethod.valueOf(requestComponents[0]);
-                path = requestComponents[1];
-            }
-            heading = new HashMap<>();
-            String headerLine;
-            while ((headerLine = in.readLine()) != null && !headerLine.isEmpty()) {
-                String[] headerComponents = headerLine.split(HEADER_DELIMITER, 2);
-                heading.put(headerComponents[0].trim(), headerComponents[1].trim());
-            }
-            StringBuilder payload = new StringBuilder();
-            if (in.ready()) {
-                int contentLength = Integer.parseInt(heading.getOrDefault("Content-Length", "0"));
-                if (contentLength > 0) {
-                    char[] buffer = new char[contentLength];
-                    in.read(buffer, 0, contentLength);
-                    payload.append(buffer);
-                    body=payload.toString();
-                }else{
-                    body = "";
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-
-    public HttpMethod getMethodType() {
-        return methodType;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public Map<String, String> getHeading() {
-        return heading;
-    }
-
-    public String getBody() {
-        return body;
-    }
 }
-
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,12 +1,12 @@
 package ru.netology;
 
 
-import javax.crypto.spec.PSource;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,12 +15,11 @@ public class Server implements Runnable {
     private static final ConcurrentHashMap<Map<HttpMethod, String>, Handler> handlerMap = new ConcurrentHashMap<>();
     static List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     private final Socket socket;
-
+    HttpRequest httpRequest;
 
     public Server(Socket socket) {
         this.socket = socket;
     }
-
 
     static void addHandler(HttpMethod methodType, String path, Handler handler) {
         handlerMap.put(Map.of(methodType, path), handler);
@@ -29,33 +28,58 @@ public class Server implements Runnable {
     @Override
     public void run() {
 
-        try (final var out = new BufferedOutputStream(socket.getOutputStream())) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream())
+        ) {
+            String requestLine = in.readLine();
+            if (requestLine != null) {
+                String[] requestComponents = requestLine.split(" ");
+                HttpMethod method = HttpMethod.valueOf(requestComponents[0]);
+                String path = requestComponents[1];
 
-            HttpRequest httpRequest = new HttpRequest(socket);
-
+                Map<String, String> headers = new HashMap<>();
+                String headerLine;
+                while ((headerLine = in.readLine()) != null && !headerLine.isEmpty()) {
+                    String[] headerComponents = headerLine.split(":", 2);
+                    headers.put(headerComponents[0].trim(), headerComponents[1].trim());
+                }
+                // Чтение тела запроса (если есть)
+                StringBuilder payload = new StringBuilder();
+                if (in.ready()) {
+                    int contentLength = Integer.parseInt(headers.getOrDefault("Content-Length", "0"));
+                    if (contentLength > 0) {
+                        char[] buffer = new char[contentLength];
+                        in.read(buffer, 0, contentLength);
+                        payload.append(buffer);
+                    }
+                }
+                this.httpRequest = new HttpRequest(method, path, headers, payload.toString());
+            }
             for (Map.Entry<Map<HttpMethod, String>, Handler> entry : handlerMap.entrySet()) {
                 Map<HttpMethod, String> mapKey = entry.getKey();
 
                 for (HttpMethod method : mapKey.keySet()) {
-                    if (method.equals(httpRequest.getMethodType())) {
+                    if (method.equals(this.httpRequest.methodType())) {
                         String path;
-                        if (mapKey.get(method).equals(httpRequest.getPath())) {
+                        if (mapKey.get(method).equals(this.httpRequest.path())) {
                             path = mapKey.get(method);
                             Handler handler = handlerMap.get(Map.of(method, path));
                             handler.handle(httpRequest, out);
+                            return;
                         } else {
 
                         }
                     }
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println(e.getMessage());
         }
     }
 }
+
+
 
 
 
